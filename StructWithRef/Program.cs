30baca259418f2407d3d76a8e27d96be494d1d05
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-18 23:38:04 +0100

    Added files to the project

--- StructWithRef/Program.cs
+++ StructWithRef/Program.cs
@@ -2,11 +2,35 @@
 
 namespace StructWithRef
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Wrapper w = new Wrapper();
+            w.nums = new int[3] { 10, 20, 30 };
+            UpdateArray(w);
+            PrintArray(w);
+            NewArray(w);
+            PrintArray(w);
+        }
+
+        private static void UpdateArray(Wrapper w)
+        {
+            w.nums[1] = 200;
+        }
+
+        private static void NewArray(Wrapper w)
+        {
+            w.nums = new int[3] { 1, 2, 3 };
+        }
+
+        private static void PrintArray(Wrapper w)
+        {
+            foreach (int i in w.nums)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
         }
     }
 }

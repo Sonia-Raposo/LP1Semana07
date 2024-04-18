f4579549afc235a82e9aadd12ecb6483a0ef4d3f
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-18 23:28:06 +0100

    Added files to the project

--- OverrideVsNew/Program.cs
+++ OverrideVsNew/Program.cs
@@ -2,11 +2,21 @@
 
 namespace OverrideVsNew
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Animal a0 = new Animal();
+            Animal a1 = new Dog();
+            Animal a2 = new Cat();
+            Animal a3 = new Elephant();
+            Elephant e = new Elephant();
+
+            Console.WriteLine($"a0 (Animal)   : {a0.Sound()}");
+            Console.WriteLine($"a1 (Dog)      : {a1.Sound()}");
+            Console.WriteLine($"a2 (Cat)      : {a2.Sound()}");
+            Console.WriteLine($"a3 (Elephant) : {a3.Sound()}");
+            Console.WriteLine($" e (Elephant) : {e.Sound()}");
         }
     }
 }

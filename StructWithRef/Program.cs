e67a74affed5834006ccfd1c8a79a306810a2766
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-18 23:35:48 +0100

    Added StructWithRef project to the solution

---
+++ StructWithRef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructWithRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

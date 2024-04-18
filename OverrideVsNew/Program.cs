40bb77161c1671f73038d43d48c1789e6b23df60
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-18 23:27:26 +0100

    Added OverrideVsNew solution to the project

---
+++ OverrideVsNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverrideVsNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

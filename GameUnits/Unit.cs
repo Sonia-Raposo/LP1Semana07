a3cbe21c2f5ab469187f9cc9d4759eef0105adf9
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-19 13:59:42 +0100

    Created Move method Cost property + Cost abstract + Class abstract + Health virtual

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.Design.Serialization;
+
 namespace GameUnits
 {
-    public class Unit
+    public abstract class Unit
     {
         private int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
+        public abstract float Cost { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
         }
+
+        public string Move(int movement)
+        {
+            return $"{this.movement = movement}";
+        }
+
     }
 }

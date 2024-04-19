43dfd3d15860d1c9db76f7035e05c6b061d933ae
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-19 14:40:18 +0100

    Override property Cost and Health

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -5,8 +5,32 @@ namespace GameUnits
         public int AttackPower { get; }
         public int XP { get; private set; }
 
-        public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
-        {                                                        // Unit(int, int)
+        private float _cost;
+        
+        public override int Health 
+        {
+            set
+            {
+                Health = base.Health + XP;
+            }
+
+            get
+            {
+                return Health;
+            }
+        }
+
+        public override float Cost
+        {
+            get
+            {
+                return _cost = AttackPower + XP;
+            }
+        }
+
+        public MilitaryUnit(int mov, int health, int attackPower)
+        : base(mov, health)
+        {                                                // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
         }
